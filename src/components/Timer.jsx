d297614de61835f5321cb57c1f2e0fcaa4d09b05
Daojim <jimmyddao@gmail.com>
2025-05-09 19:38:34 -0400

    Add start/pause button for Timer component

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,7 +1,21 @@
 function Timer() {
+  function onStartPauseClick() {
+    alert("clicked");
+  }
+
+  const startingMinutes = 25;
+  let time = startingMinutes * 60;
+
   return (
-    <div>
-      <p>Timer</p>
+    <div className="countdownCard">
+      <div className="countdownTimer">
+        Timer
+        <div className="startPause">
+          <button className="startPause-btn" onClick={onStartPauseClick}>
+            Start
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
