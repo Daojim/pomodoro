dffc78b3c01faa8c8744f58f5c45b9505c3f2800
Daojim <jimmyddao@gmail.com>
2025-05-09 22:10:11 -0400

    Add comments

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,22 +1,23 @@
 import { useState, useRef, useEffect } from "react";
 
 function Timer() {
-  const [timeText, setTimeText] = useState("25:00");
-  const [isCountingDown, setIsCountingDown] = useState(false);
+  const [timeText, setTimeText] = useState("25:00"); // State for timer text
+  const [isCountingDown, setIsCountingDown] = useState(false); // State for counting down but using it to change the button between Start/Pause
   const [count, setCount] = useState(1000);
-  let startingMinutes = 25;
-  let time = startingMinutes * 60;
+  let startingMinutes = 25; // Setting default timer of pomodoro to be 25 minutes
+  let time = startingMinutes * 60; // 25 minutes * 60 to get total amount of seconds
 
   function onStartPauseClick() {
-    setIsCountingDown((prevState) => !prevState);
+    setIsCountingDown((prevState) => !prevState); // Switches to previous state
   }
 
   useEffect(() => {
+    //geeksforgeeks code idea
     console.log("useEffect runs");
 
     const interval = setInterval(() => {
       time--;
-      const minutes = Math.floor(time / 60);
+      const minutes = Math.floor(time / 60); // Get the number of minutes by dividing total seconds calculated earlier
       let seconds = time % 60;
       setCount((prevCount) => prevCount - 1);
       setTimeText(`${minutes}:${seconds}`);
