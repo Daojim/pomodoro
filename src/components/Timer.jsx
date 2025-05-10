3f039608f50e18a02206968db7c36e1245cbbcd7
Daojim <jimmyddao@gmail.com>
2025-05-09 20:52:23 -0400

    Remove setInterval to stop timer from glitching

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 function Timer() {
   const startingMinutes = 25;
   let time = startingMinutes * 60;
   const [timeText, setTimeText] = useState("25:00");
-
-  setInterval(updateCountdown, 1000);
 
   function updateCountdown() {
     const minutes = Math.floor(time / 60);
