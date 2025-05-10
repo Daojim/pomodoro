9b411b83ab2021e30af63cccb27e61c9da2299ef
Daojim <jimmyddao@gmail.com>
2025-05-09 21:03:18 -0400

    Remove timer logic to start over

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,26 +1,14 @@
 import { useState, useRef, useEffect } from "react";
 
 function Timer() {
-  const startingMinutes = 25;
-  let time = startingMinutes * 60;
   const [timeText, setTimeText] = useState("25:00");
-
-  function updateCountdown() {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-
-    setTimeText(`${minutes}:${seconds}`);
-    time--;
-  }
 
   return (
     <div className="timerCard">
       <div className="timerSection">
         <h1 className="countdown">{timeText}</h1>
         <div className="startPause">
-          <button className="startPause-btn" onClick={updateCountdown}>
-            Start
-          </button>
+          <button className="startPause-btn">Start</button>
         </div>
       </div>
     </div>
