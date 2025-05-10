92ff732c05bfce7bf9494690bf04ef8455b54035
Daojim <jimmyddao@gmail.com>
2025-05-10 12:40:48 -0400

    Reworked timer logic to countdown and pause at 0

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,24 +1,46 @@
 import { useState, useEffect, useRef } from "react";
 
 function Timer() {
-  const [timeText, setTimeText] = useState("25:00"); // State for timer text
+  const [timeText, setTimeText] = useState("1:00"); // State for timer text
   const [isCountingDown, setIsCountingDown] = useState(false); // State for counting down but using it to change the button between Start/Pause
-  const startingMinutes = 25; // Setting default timer of pomodoro to be 25 minutes
-  let time = startingMinutes * 60; // 25 minutes * 60 to get total amount of seconds
+  const time = useRef(1 * 60); //useRef doesn't reset on re-render or cause re-renders when updated
+  const intervalId = useRef(null); //null is blank/initial value
 
   function onStartPauseClick() {
     setIsCountingDown((prevState) => !prevState); // Switches to previous state
   }
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      time--;
-      const minutes = Math.floor(time / 60); // Get the number of minutes by dividing total seconds calculated earlier
-      let seconds = time % 60;
-      setTimeText(`${minutes}:${seconds < 10 ? "0" : ""}${seconds}`); //If seconds is less than 10, add a leading zero
-    }, 1000);
+  // previous
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     time--;
+  //     const minutes = Math.floor(time / 60); // Get the number of minutes by dividing total seconds calculated earlier
+  //     let seconds = time % 60;
+  //     setTimeText(`${minutes}:${seconds < 10 ? "0" : ""}${seconds}`); //If seconds is less than 10, add a leading zero
+  //   }, 1000);
 
-    return () => clearInterval(interval);
+  //   return () => clearInterval(interval);
+  // }, [isCountingDown]);
+
+  useEffect(() => {
+    if (isCountingDown) {
+      intervalId.current = setInterval(() => {
+        //If time hits 0, the timer stops
+        if (time.current <= 0) {
+          clearInterval(intervalId.current);
+          return;
+        }
+
+        time.current--;
+        const minutes = Math.floor(time.current / 60);
+        const seconds = time.current % 60;
+        setTimeText(`${minutes}:${seconds < 10 ? "0" : ""}${seconds}`);
+      }, 1000);
+    } else {
+      clearInterval(intervalId.current);
+    }
+
+    return () => clearInterval(intervalId.current);
   }, [isCountingDown]);
 
   return (
