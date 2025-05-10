42cc0c20bd0d3775683e9e0d1e3eb78ff7d24d65
Daojim <jimmyddao@gmail.com>
2025-05-09 20:44:33 -0400

    Timer counts down but bugs out graphically

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -5,31 +5,26 @@ function Timer() {
     alert("clicked");
   }
 
-  //   const countdown = document.querySelector(".countdown");
+  const startingMinutes = 25;
+  let time = startingMinutes * 60;
+  const [timeText, setTimeText] = useState("25:00");
 
-  //   const startingMinutes = 25;
-  //   let time = startingMinutes * 60;
+  setInterval(updateCountdown, 1000);
 
-  //   setInterval(updateCountdown, 1000);
+  function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
 
-  //   function updateCountdown() {
-  //     const minutes = Math.floor(time / 60);
-  //     let seconds = time % 60;
-
-  //     time--;
-  //   }
-
-  const [timeText, setTimeText] = useState("hello");
+    setTimeText(`${minutes}:${seconds}`);
+    time--;
+  }
 
   return (
     <div className="timerCard">
       <div className="timerSection">
         <h1 className="countdown">{timeText}</h1>
         <div className="startPause">
-          <button
-            className="startPause-btn"
-            onClick={() => setTimeText("TEST STATE CHANGE")}
-          >
+          <button className="startPause-btn" onClick={updateCountdown}>
             Start
           </button>
         </div>
