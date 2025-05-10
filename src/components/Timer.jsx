7cc631486607682b40f8e7aabb043a4760dd94a0
Daojim <jimmyddao@gmail.com>
2025-05-09 21:10:22 -0400

    Add State for Pause/Start button Learned that I could go back to previous state (prevState) if state changes https://react.dev/reference/react/Component#getsnapshotbeforeupdate-parameters

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,13 +2,20 @@ import { useState, useRef, useEffect } from "react";
 
 function Timer() {
   const [timeText, setTimeText] = useState("25:00");
+  const [isCountingDown, setIsCountingDown] = useState(false);
+
+  function onStartPauseClick() {
+    setIsCountingDown((prevState) => !prevState);
+  }
 
   return (
     <div className="timerCard">
       <div className="timerSection">
         <h1 className="countdown">{timeText}</h1>
         <div className="startPause">
-          <button className="startPause-btn">Start</button>
+          <button className="startPause-btn" onClick={onStartPauseClick}>
+            {isCountingDown ? "Pause" : "Start"}
+          </button>
         </div>
       </div>
     </div>
