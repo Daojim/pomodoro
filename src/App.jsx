67ee212379627d892bad3e5b757811a7d6bde651
Daojim <jimmyddao@gmail.com>
2025-05-10 21:24:42 -0400

    Add links to my github and linkedin

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,22 @@ function App() {
     <>
       <h1>Jimmy's Simple Pomodoro App</h1>
       <Timer />
+      <div className="socialLinks">
+        <a
+          href="https://github.com/Daojim"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jimmy-d-9a0156130/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </div>
     </>
   );
 }
